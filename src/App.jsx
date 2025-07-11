2f6f58edb02c4afc97e851b5c16773db1da33983
hicksey <hicksey14@gmail.com>
2025-07-10 19:02:33 -0600

    add react-tutorial course index.css code, remove no longer needed App.css file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Product from './Product.jsx';
-import './App.css'
 
 function App(props) {
   const [count, setCount] = useState(0);
